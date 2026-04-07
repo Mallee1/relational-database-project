357552a462ac5d9cbde28a5e59c5364809a265fe
Max Allee <150299064+Mallee1@users.noreply.github.com>
2026-04-07 09:46:40 -0400

    Add SQL query to summarize employee service hours

---
+++ sql/queries.sql
@@ -0,0 +1,25 @@
+SELECT 
+    EMPLOYEE.EmployeeID,
+    EMPLOYEE.ExperienceLevel,
+    PROPERTY.PropertyID,
+    PROPERTY.City,
+    PROPERTY.State,
+    PROPERTY.Zip,
+    SUM(SERVICE_HOURS.HoursWorked) AS TotalHoursWorked
+FROM 
+    (SERVICE_HOURS 
+    INNER JOIN EMPLOYEE ON SERVICE_HOURS.EmployeeID = EMPLOYEE.EmployeeID)
+    INNER JOIN PROPERTY ON SERVICE_HOURS.PropertyID = PROPERTY.PropertyID
+GROUP BY 
+    EMPLOYEE.EmployeeID,
+    EMPLOYEE.ExperienceLevel,
+    PROPERTY.PropertyID,
+    PROPERTY.City,
+    PROPERTY.State,
+    PROPERTY.Zip
+ORDER BY 
+    EMPLOYEE.EmployeeID,
+    EMPLOYEE.ExperienceLevel,
+    PROPERTY.State,
+    PROPERTY.City,
+    PROPERTY.Zip;
